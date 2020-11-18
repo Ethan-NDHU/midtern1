10035b3d12d7feaf35bf2b163b7516c67ff26c17
410696016_Ethan <71751245+Ethan-NDHU@users.noreply.github.com>
2020-11-18 14:24:32 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,29 @@
 input.onButtonPressed(Button.A, function () {
-	
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . # . .
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . # . # .
+        . . . . .
+        # . . . #
+        # . . . #
+        . # # # .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        `)
 })
 basic.forever(function () {
 	
